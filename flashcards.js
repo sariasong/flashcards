7a1795de83cded24c107d70c82b0f2c4064e4789
Sarah Daenitz <sarahdaenitz@gmail.com>
2018-03-06 17:23:33 -0700

    flash

---
+++ flashcards.js
@@ -0,0 +1,18 @@
+// $(document).ready(function(){
+//   $("p").hide();
+//   $("#btn").click(function(){
+//     $("p").toggle();
+//   })
+// })
+// $(document).ready(function(){
+//   $("p").hide();
+// });
+
+// for (var i = 0; i < buttons.length; i++) {
+//   buttons[i].addEventListener('click', clickButton, false);
+// };
+
+$("#btn").click( function(){
+  $("#card").hide();
+  console.log("something")
+});
